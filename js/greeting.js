08c388fd7c229fa786235578562102729c13dc01
LeeJinWoo <dlwlsdn201@naver.com>
2021-01-26 11:53:55 +0900

    {feature}: <인사 comment, ToDoList 목록 기능 추가>
    <description>
    - 사용자 이름 입력 시 인사말 comment 표시되는 js 코드 추가
    - To Do 내용 입력 시 li 형태로 표시되는 js 코드 추가
    - To Do 내용 삭제 버튼 추가
    
    <Feature-#001>

---
+++ js/greeting.js
@@ -0,0 +1,52 @@
+const FORM = document.querySelector('#js-UserForm'),
+INPUT = FORM.querySelector("input"),
+GREETING = document.querySelector('#js-Greeting');
+
+const userKey = 'currentUser',
+SHOWING = 'showing';
+
+
+// INPUT에 입력된 값 로드 & 사용자 이름 저장 & 환영인사말 표시
+handleSubmit = (event) => {
+    event.preventDefault();
+
+    const currentValue = INPUT.value;
+    drawGreeting(currentValue);
+    saveName(currentValue);
+}
+
+
+// 사용자 이름을 로컬 스토리지에 key값과 함께 저장
+saveName = (value) => {
+    localStorage.setItem(userKey, value);
+}
+
+//userForm class에 'showing'을 추가
+//userForm submit 이벤트 발생 시 hnadleSubmit 호출
+askForName = () => {
+    FORM.classList.add(SHOWING);
+    FORM.addEventListener("submit", handleSubmit);
+}
+
+//userForm의 showing class 삭제
+// 인사말 요소에 텍스트 삽입
+drawGreeting = (text) => {
+    FORM.classList.remove(SHOWING);
+    GREETING.classList.add(SHOWING);
+    GREETING.innerText = `Hello ${text}`;
+}
+
+//로컬스토리지에서 userName value 로드하여 drawGreeting 함수에 전달
+loadName = () => {
+    const currentUser = localStorage.getItem(userKey);
+    if(currentUser === null)
+        askForName();
+    else
+        drawGreeting(currentUser);
+}
+
+function init(){
+    loadName();
+};
+
+init();
---
+++ js/toDoList.js
@@ -0,0 +1,76 @@
+const toDoForm = document.querySelector('#js-toDoForm'), 
+toDoInput = toDoForm.querySelector('input'),
+toDoList = document.querySelector('#toDoList');
+
+const ToDo_LocalStorage = "toDos";
+let toDos = [];
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+deleteToDo = (event) => {
+    console.log('remove ',event.target)
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    //filter 함수를 사용하여 삭제 li 요소 외 나머지 li들로 새로운 배열을 생성.
+    const cleanToDos = toDos.filter((toDo)=>{
+        return toDo.id !== li.id;
+    });
+    toDos = cleanToDos;
+    saveToDos();
+
+}
+
+//로컬 스토리지에 string 형태로 배열을 저장.
+saveToDos = () => {
+    localStorage.setItem(ToDo_LocalStorage, JSON.stringify(toDos));
+}
+
+paintToDo = (text) => {
+    const li = document.createElement('li');
+    const delBtn = document.createElement('button');
+    delBtn.innerHTML = "X";
+    delBtn.addEventListener('click',deleteToDo);
+    const span = document.createElement('span');
+    const newId = toDos.length + 1;
+
+    // ul 태그에 toDo li 태그를 추가
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDoList.appendChild(li);
+
+
+    // toDo 각각을 객체로 만들어서 로컬스토리지에 저장할 배열에 삽입
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+loadToDos = () => {
+    const loadedToDos = localStorage.getItem(ToDo_LocalStorage);
+
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach((toDo)=>{
+            paintToDo(toDo.text);
+        })
+    }
+}
+
+init = () => {
+    loadToDos();
+    toDoForm.addEventListener("submit",handleSubmit)
+}
+
+init()
