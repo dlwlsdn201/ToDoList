f463e33a9e11bd683d6f5c2c10733cebd5090f06
LeeJinWoo <dlwlsdn201@naver.com>
2021-01-27 17:22:16 +0900

    add background js-file. closed #3

---
+++ js/background.js
@@ -0,0 +1,28 @@
+const BODY = document.querySelector("body");
+
+const IMG_NUMBER = 5;
+
+handleImgLoad = () => {
+    console.log('finished backgruond loading!');
+}
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `../images/bg/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    BODY.appendChild(image);
+}
+
+setRandomNumber = () =>{
+    const number = Math.floor(Math.random() * IMG_NUMBER); 
+
+    return number;
+}
+
+
+function init(){
+    const randomNumber = setRandomNumber();
+    paintImage(randomNumber);
+}
+
+init();
