97f3d5ecde40beca0eb1f78bd3dd2968daba56c6
LeeJinWoo <dlwlsdn201@naver.com>
2021-01-29 14:22:45 +0900

    {Feature}: <날씨 정보 제공 기능 구현>
    <description>
    - 사용자의 현재 위치(위도,경도) 정보 + 기상 홈페이지 API을 이용하여
    사용자의 현재 위치 날씨 정보를 제공하는 기능 구현.
    
    <issue-number>
    feature #2
    close #2

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,21 @@
+const WEHATER = document.querySelector("#js-weather");
+const API_KEY = 'b8dbfe24ff72d50050db7e51e0dd5b77';
 const COORDS = 'coords';
+
+function getWeather(lat, lng){
+// fetch를 이용하여 데이터 가져오기
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(
+        function(response){
+            return response.json();
+        }).then(function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            
+            WEHATER.innerText = `${temperature}℃ ${place}`
+            console.log(json);
+        }
+        );
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -11,8 +28,8 @@ function handleGeoSuccess(position){
         latitude,
         longitude
     };
-
     saveCoords(coordsObj);
+    // getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -25,11 +42,12 @@ function askForCoord(){
 
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoord();
     }else{
-        //getweather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
