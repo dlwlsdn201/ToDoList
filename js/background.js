e5863a875db61a82cfd15678b4cead6a24f7d3e9
LeeJinWoo <dlwlsdn201@naver.com>
2021-03-29 10:55:20 +0900

    모바일 기기용 배경화면 이미지 추가 완료_ closed #41

--- js/background.js
+++ js/background.js
@@ -7,7 +7,14 @@ handleImgLoad = () => {
 }
 
 
-function paintImage(imgNumber){
+paintImageMobile = (imgNumber) => {
+    const image = new Image();
+    image.src = `./images/mobile/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    BODY.appendChild(image);
+}
+
+paintImage = (imgNumber) => {
     const image = new Image();
     image.src = `./images/bg/${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
@@ -23,7 +30,10 @@ setRandomNumber = () =>{
 
 function init(){
     const randomNumber = setRandomNumber();
-    paintImage(randomNumber);
+    const deviceWidth = window.screen.width;
+
+    //디바이스 스크린 크기에 따른 배경화면 적용
+    (deviceWidth > 500? paintImage(randomNumber) : paintImageMobile(randomNumber));
 }
 
 init();
